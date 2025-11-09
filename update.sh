1860265d90350c1a383f5f2178a6b353ee235ae0
mstroh <m.stroh76@gmail.com>
2025-11-09 10:26:21 +0100

    public fix

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
+echo --- prepare folder cache ---
 killall hugo
 #rm -rf public
 rm -rf /dev/shm/public
-ln -s /dev/shm/public public
+#ln -s /dev/shm/public public
 
+echo --- rsync local ---
 rsync -v ../Blog/config.toml config.toml
 rsync -rv --delete ../Blog/content/* content
 rsync -rv --delete ../Blog/static/* static
+
+echo --- run hugo ---
 hugo
 chromium "http://localhost:1313/Blog/" &
 hugo -D server
